37c785efd64630406b2b9c1e54f28ee272eebfc1
MaThUZiB <mathuzib@gmail.com>
2025-10-05 19:39:53 -0300

    movimiento personaje

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -3,12 +3,13 @@ using UnityEngine.InputSystem;
 
 public class MovimientoPersonaje : MonoBehaviour
 {
+    private SpriteRenderer spritePersonaje;
+    private Animator animator;
     public float velocidad = 10f;
     public float fuerzaSalto = 12f;
     public Transform chequeoSuelo;
     public float radioChequeo = 0.12f;
     public LayerMask capaSuelo;
-
     private bool enSuelo;
     private Vector2 moveInput;
     private Rigidbody2D rb;
@@ -16,8 +17,9 @@ public class MovimientoPersonaje : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spritePersonaje = GetComponent<SpriteRenderer>();
     }
-
     public void OnJump(InputAction.CallbackContext context)
     {
         if (context.performed && enSuelo)
@@ -35,6 +37,18 @@ public class MovimientoPersonaje : MonoBehaviour
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(moveInput.x * velocidad, rb.linearVelocity.y);
+        animator.SetFloat("camina", Mathf.Abs(moveInput.x * velocidad));
+        animator.SetBool("enSuelo", enSuelo);
+
+        if (moveInput.x * velocidad < 0)
+        {
+            spritePersonaje.flipX = true;
+        }
+        else if (moveInput.x * velocidad > 0)
+        {
+            spritePersonaje.flipX = false;
+        }
+
         enSuelo = Physics2D.OverlapCircle(chequeoSuelo.position, radioChequeo, capaSuelo);
     }
 }
